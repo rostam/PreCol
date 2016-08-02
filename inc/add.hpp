df83390e7aa04687c7fcde291786a95a06e81d2b
Ali <a.rostami@uni-jena.de>
2016-08-02 12:24:30 +0200

    small correction

--- inc/add.hpp
+++ inc/add.hpp
@@ -132,7 +132,7 @@ int addReqElements(Graph& G_b, vector<Edge>& edge_ordering) {
  *
  * \return number of additionally required elements
  */
-int addReqElementsWeak(Graph& G_b, vector<graph_traits<Graph>::edge_descriptor>& edge_ordering) {
+int addReqElementsWeak(Graph& G_b, vector<Edge>& edge_ordering) {
     property_map<Graph, edge_weight_t>::type weight = get(edge_weight, G_b);
     property_map<Graph, edge_name_t>::type name = get(edge_name, G_b);
     graph_traits<Graph>::out_edge_iterator oe_it, oe_it_end;
