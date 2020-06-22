d8dee44ccdeded01d0fdec1364a6faa802b531e7
Ali Rostami <rostamiev@gmail.com>
2020-06-22 07:59:29 +0200

    adding pictures of matrices for better understanding

--- BoostTest/PreColTest.cpp
+++ BoostTest/PreColTest.cpp
@@ -51,16 +51,22 @@ BOOST_AUTO_TEST_SUITE(GeneratorsTestSuite)
         BOOST_CHECK_EQUAL(V_r.size(),6);
         BOOST_CHECK_EQUAL(V_c.size(),6);
         int entries_pattern = sparsifier(G_b, sparsify, mm.nrows(),blockSize);
+        BOOST_CHECK_EQUAL(entries_pattern,16);
         shared_ptr<ColAlg> ret = getAlg(Mode2, alg, Mode, G_b, V_r, V_c, col_ord_c, alpha);
         int num_of_colors = ret -> color();
         BOOST_CHECK_EQUAL(num_of_colors,6);
+
+        //std::string sparsify = "Full";
     }
-    BOOST_AUTO_TEST_CASE(ColoringTest) {
+
+
+
+    BOOST_AUTO_TEST_CASE(ArrowShapedPlusOneTest) {
         std::string alg = "D2Columns";
         std::string col_ord = "LFO";
         shared_ptr<Ordering> col_ord_c = get_color_ordering(col_ord);
         std::string pre_ord = "Nat";
-        std::string filename = "mats/arrow-shaped2.mtx";
+        std::string filename = "mats/arrow-shaped_plus_one.mtx";
         std::string sparsify = "Full";
         int blockSize = 30;
         int el = 2;
@@ -82,8 +88,42 @@ BOOST_AUTO_TEST_SUITE(GeneratorsTestSuite)
         BOOST_CHECK_EQUAL(V_r.size(),6);
         BOOST_CHECK_EQUAL(V_c.size(),6);
         int entries_pattern = sparsifier(G_b, sparsify, mm.nrows(),blockSize);
+        BOOST_CHECK_EQUAL(entries_pattern,17);
         shared_ptr<ColAlg> ret = getAlg(Mode2, alg, Mode, G_b, V_r, V_c, col_ord_c, alpha);
         int num_of_colors = ret -> color();
         BOOST_CHECK_EQUAL(num_of_colors,6);
+    }
+
+    BOOST_AUTO_TEST_CASE(ArrowShapedPlusOneMinusOneTest) {
+        std::string alg = "D2Columns";
+        std::string col_ord = "LFO";
+        shared_ptr<Ordering> col_ord_c = get_color_ordering(col_ord);
+        std::string pre_ord = "Nat";
+        std::string filename = "mats/arrow-shaped_plus_one_minus_one.mtx";
+        std::string sparsify = "Full";
+        int blockSize = 30;
+        int el = 2;
+        int Mode = 0;
+        int Mode2 = 0;
+        int alpha = 10;
+        auto input = make_tuple(alg,col_ord_c,pre_ord,Mode,Mode2,sparsify, blockSize, el,filename,alpha);
+        matrix_market mm(filename.c_str());
+        BOOST_CHECK_EQUAL(mm.nrows(),6);
+        BOOST_CHECK_EQUAL(mm.issym(),0);
+        BOOST_CHECK_EQUAL(mm.nnz(),16);
+        Graph G_b(2 * mm.nrows());
+        //Add edges to graph
+        mm.MtxToBipGraph(G_b);
+        BOOST_CHECK_EQUAL(num_vertices(G_b),12);
+        BOOST_CHECK_EQUAL(num_edges(G_b),16);
+        vector<unsigned int> V_r, V_c;
+        for_each_v(G_b, [&](const unsigned int vi) { vi < mm.nrows() ? V_r.push_back(vi) : V_c.push_back(vi); });
+        BOOST_CHECK_EQUAL(V_r.size(),6);
+        BOOST_CHECK_EQUAL(V_c.size(),6);
+        int entries_pattern = sparsifier(G_b, sparsify, mm.nrows(),blockSize);
+        BOOST_CHECK_EQUAL(entries_pattern,16);
+        shared_ptr<ColAlg> ret = getAlg(Mode2, alg, Mode, G_b, V_r, V_c, col_ord_c, alpha);
+        int num_of_colors = ret -> color();
+        BOOST_CHECK_EQUAL(num_of_colors,5);
     }
 BOOST_AUTO_TEST_SUITE_END()
---
+++ mats/readme.md
@@ -0,0 +1,9 @@
+# Test matrices
+
+- arrow-shaped.mtx
+- arrow-shaped_plus_one.mtx
+
+![MatVis1](arrow-shaped_plus_one.png)
+- arrow-shaped_plus_one_minus_one.mtx
+
+![MatVis1](arrow-shaped_plus_one_minus_one.png)
