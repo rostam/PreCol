1b32453658a26121de1e3240657919592aceb600
Ali Rostami <rostamiev@gmail.com>
2020-07-27 16:26:55 +0200

    using auto to reduce the size of code

--- Algorithms/ISetVariantRestricted.hpp
+++ Algorithms/ISetVariantRestricted.hpp
@@ -23,7 +23,7 @@ class ISetVariantRestricted : public IndependentSet{
 public:
 	using IndependentSet::IndependentSet;
 
-    vector<int> compute()
+    vector<int> compute() override
     {
         vector<int> IS;
         list<pair<unsigned int,unsigned int> > Degree_V_r;
@@ -31,27 +31,14 @@ public:
 
         remove_edge_if(has_weight_equal_to(0, G_b),G_b);
 
-        for (vector<unsigned int>::iterator v_r = V_r.begin();
-             v_r != V_r.end();
-             ++v_r) {
+        for (auto v_r : V_r) Degree_V_r.emplace_back(make_pair(v_r,degree(v_r,G_b)));
 
-            Degree_V_r.push_back(make_pair(*v_r,degree(*v_r,G_b)));
-        }
-
-        for (vector<unsigned int>::iterator v_c = V_c.begin();
-             v_c != V_c.end();
-             ++v_c) {
-
-            Degree_V_c.push_back(make_pair(*v_c,degree(*v_c,G_b)));
-        }
+        for (auto v_c : V_c) Degree_V_c.emplace_back(make_pair(v_c,degree(v_c,G_b)));
 
         while(num_edges(G_b)>0) {
 
             unsigned int max_degree_V_r = 0;
-            for (list<pair<unsigned int,unsigned int> >::iterator di =
-                    Degree_V_r.begin();
-                 di != Degree_V_r.end();
-                 ++di) {
+            for (auto di = Degree_V_r.begin();di != Degree_V_r.end();++di) {
 
                 (*di).second = degree((*di).first,G_b);
                 unsigned int degree_v_r = (*di).second;
@@ -61,13 +48,9 @@ public:
             }
 
             unsigned int max_degree_V_c = 0;
-            for (list<pair<unsigned int,unsigned int> >::iterator di =
-                    Degree_V_c.begin();
-                 di != Degree_V_c.end();
-                 ++di) {
-
-                (*di).second = degree((*di).first,G_b);
-                unsigned int degree_v_c = (*di).second;
+            for (auto di : Degree_V_c) {
+                di.second = degree(di.first,G_b);
+                unsigned int degree_v_c = di.second;
                 if (degree_v_c > max_degree_V_c) {
                     max_degree_V_c = degree_v_c;
                 }
@@ -75,11 +58,7 @@ public:
 
             if (max_degree_V_r > ratio*max_degree_V_c) {
 
-                for (list<pair<unsigned int,unsigned int> >::iterator di =
-                        Degree_V_r.begin();
-                     di != Degree_V_r.end();
-                     ++di) {
-
+                for (auto di = Degree_V_r.begin();di != Degree_V_r.end();++di) {
                     if (max_degree_V_r==(*di).second) {
                         clear_vertex((*di).first,G_b);
                         di = Degree_V_r.erase(di); --di;
@@ -87,11 +66,7 @@ public:
                 }
             } else {
 
-                for (list<pair<unsigned int,unsigned int> >::iterator di =
-                        Degree_V_c.begin();
-                     di != Degree_V_c.end();
-                     ++di) {
-
+                for (auto di = Degree_V_c.begin();di != Degree_V_c.end();++di) {
                     if (max_degree_V_c==(*di).second) {
                         clear_vertex((*di).first,G_b);
                         di = Degree_V_c.erase(di); --di;
@@ -100,25 +75,12 @@ public:
             }
         }
 
-        for (list<pair<unsigned int,unsigned int> >::iterator di =
-                Degree_V_r.begin();
-             di != Degree_V_r.end();
-             ++di) {
+        for (auto di : Degree_V_r) IS.emplace_back(di.first);
 
-            IS.push_back((*di).first);
-        }
-
-        for (list<pair<unsigned int,unsigned int> >::iterator di =
-                Degree_V_c.begin();
-             di != Degree_V_c.end();
-             ++di) {
-
-            IS.push_back((*di).first);
-        }
+        for (auto di : Degree_V_c) IS.emplace_back(di.first);
 
         return IS;
     }
-
 };
 
 #endif
